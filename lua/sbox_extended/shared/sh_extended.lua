6d21ebec8e153cb27e54c5037539cc68884bf124
Nyaaa <5182757+oNyaaa1@users.noreply.github.com>
2026-01-12 19:04:54 +0000

    added extended for doors

--- lua/sbox_extended/shared/sh_extended.lua
+++ lua/sbox_extended/shared/sh_extended.lua
@@ -220,6 +220,22 @@ local positions = {
         if num == 2 then return Vector(62, -25, 8), 90 end
         if num == 3 then return Vector(-25, -62, 8), 0 end
         if num == 4 then return Vector(-62, -25, 8), 90 end
+    end,
+    ["sent_door_metal"] = function(ply)
+        local text = math.Round(360 - ((ply:GetAngles().y - 360) % 360))
+        local num = ENUM_LOOK_AT(text)
+        if num == 1 then return Vector(-25, 62, 8), 0 end
+        if num == 2 then return Vector(62, -25, 8), 90 end
+        if num == 3 then return Vector(-25, -62, 8), 0 end
+        if num == 4 then return Vector(-62, -25, 8), 90 end
+    end,
+    ["sent_door_metal_armoured"] = function(ply)
+        local text = math.Round(360 - ((ply:GetAngles().y - 360) % 360))
+        local num = ENUM_LOOK_AT(text)
+        if num == 1 then return Vector(-25, 62, 8), 0 end
+        if num == 2 then return Vector(62, -25, 8), 90 end
+        if num == 3 then return Vector(-25, -62, 8), 0 end
+        if num == 4 then return Vector(-62, -25, 8), 90 end
     end
 }
 
